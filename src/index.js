9da304945f30a9de0a8e82c17ba79d24b28c5634
Rishant Rana <kiturana378@gmail.com>
2025-02-01 15:13:15 +0530

    utils discussion

--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ dotenv.config()
 import connectDB from "./db/db.js";
 
 
-
-connectDB();
+//connectDB is a promise and it means we can write .then and .catch
+connectDB().
+then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log("Server listening on port " + process.env.PORT); 
+    })
+}).
+catch((err)=>{
+    console.log("MONGO ERROR: " + err);
+    
+})
